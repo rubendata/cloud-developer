e0491f92f57b7815cde1b68750892e323931ec85
Ruben Simon <info@rubensimon.de>
2020-08-11 21:27:00 +0200

    added unit test with mocha excercise

--- Course 2/udacity-c2-basic-server/src/unit-test-examples/units.tests.ts
+++ Course 2/udacity-c2-basic-server/src/unit-test-examples/units.tests.ts
@@ -1,4 +1,4 @@
-import { add, divide } from './units';
+import { add, divide, concat } from './units';
 
 import { expect } from 'chai';
 import 'mocha';
@@ -38,3 +38,17 @@ describe('divide', () => {
 // @TODO try creating a new describe block for the "concat" method
 // it should contain an it block for each it statement in the units.ts @TODO.
 // don't forget to import the method ;)
+
+describe('concat', () => {
+
+ 
+  it('should combine "cloud" and "developer"', () => {
+    const result = concat("cloud","developer");
+    expect(result).to.equal("clouddeveloper");
+  });
+
+  it('should throw an error', () => {
+    expect(()=>{ concat("", "test") }).to.throw('one string is empty')
+  });
+
+});
