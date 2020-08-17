71b6922c36c8fba5c4fe8bc655039a34e2f77979
Ruben Simon <info@rubensimon.de>
2020-08-17 21:59:55 +0200

    added environment variables and adjusted gitignore

--- Course 2/udacity-c2-restapi/src/config/config.ts
+++ Course 2/udacity-c2-restapi/src/config/config.ts
@@ -1,13 +1,13 @@
 export const config = {
   "dev": {
-    "username": "udagram",
-    "password": "8JY35s1O2!",
-    "database": "udagram",
-    "host": "udagram.cokzmjqlyw72.us-east-1.rds.amazonaws.com",
+    "username": process.env.POSTGRES_USERNAME,
+    "password": process.env.POSTGRES_PASSWORD,
+    "database": process.env.POSTGRES_DATABASE,
+    "host": process.env.POSTGRES_HOST,
     "dialect": "postgres",
-    "aws_region": "eu-central-1", /*original: us-east-2*/
-    "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruben-dev"
+    "aws_region": process.env.AWS_REGION, /*original: us-east-2*/
+    "aws_profile": process.env.AWS_PROFILE,
+    "aws_media_bucket": process.env.AWS_MEDIA_BUCKET
   },
   "prod": {
     "username": "",
