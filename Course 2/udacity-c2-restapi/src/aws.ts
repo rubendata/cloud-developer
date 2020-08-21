49b3902711f3a089ef1b25ff19bfb70f53ab4018
Ruben Simon <info@rubensimon.de>
2020-08-21 21:18:14 +0200

    changed to original build code

--- Course 2/udacity-c2-restapi/src/aws.ts
+++ Course 2/udacity-c2-restapi/src/aws.ts
@@ -5,13 +5,11 @@ const c = config.dev;
 
 
 //Configure AWS
-var credentials = new AWS.SharedIniFileCredentials({profile: 'default'});
-AWS.config.credentials = credentials;
 
-// if(c.aws_profile !== "DEPLOYED") {
-//   var credentials = new AWS.SharedIniFileCredentials({profile: 'default'});
-//   AWS.config.credentials = credentials;
-// }
+if(c.aws_profile !== "DEPLOYED") {
+  var credentials = new AWS.SharedIniFileCredentials({profile: 'default'});
+  AWS.config.credentials = credentials;
+}
 
 export const s3 = new AWS.S3({
   signatureVersion: 'v4',
