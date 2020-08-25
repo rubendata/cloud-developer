ac918484dc59766f21ee7ad10c5d0ab04fe8790a
Ruben Simon <info@rubensimon.de>
2020-08-25 20:30:45 +0200

    downloaded the project code

--- Course 2/udacity-c2-frontend/src/environments/environment.prod.ts
+++ Course 2/udacity-c2-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://udacity-c2-restapi-dev.eu-central-1.elasticbeanstalk.com'
 };
 
 /*
